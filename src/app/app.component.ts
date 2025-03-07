6856842384aff05456f5c0b34327540df69e7b2f
nikhil-v-keka <nikhil.v@keka.com>
2025-03-07 19:19:24 +0530

    removed commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { saveAs } from 'file-saver';
+
 import { TranslatorService } from './services/translator.service';
 import { FileUploadDetails } from './models/file-upload-details.model';
 import { TranslateDto } from './models/translateDto.model';
-import { saveAs } from 'file-saver';
 
 
 @Component({
@@ -13,9 +14,8 @@ import { saveAs } from 'file-saver';
     templateUrl: './app.component.html',
     standalone: true,
     imports: [
-        CommonModule, 
+        CommonModule,
         FormsModule,
-        // QuillModule.forRoot()
     ],
     styleUrl: './app.component.scss'
 })
@@ -33,33 +33,11 @@ export class AppComponent {
     editorContainer: ElementRef | null = null;
 
     editor;
-    
+
     ngOnInit() {
         this.translateService.getLanguages().subscribe({
             next: (data: any) => this.languages = data.languages
         });
-        // if (this.editorContainer) {
-        //     try {
-        //         this.editor = new Quill(this.editorContainer.nativeElement, {
-        //             modules: {
-        //                 toolbar: [
-        //                     [{ header: [1, 2, false] }],
-        //                     ["bold", "italic", "underline", "strike"],
-        //                     [{ list: "ordered" }, { list: "bullet" }],
-        //                     ["link", "image", "video"],
-        //                     [{ align: [] }],
-        //                     [{ color: [] }, { background: [] }],
-        //                     ["clean"],
-        //                 ],
-        //             },
-        //             theme: "snow",
-        //         });
-        //     } catch (error) {
-        //         console.error("Error creating Quill editor:", error);
-        //     }
-        // } else {
-        //     console.error("Element with #editorContainer not found!");
-        // }
     }
 
     onFileSelected(event: any) {
